c25b2201ef234be03db7242e57fa7526a9f346cf
Lescha1989 <lescha19891@tut.by>
2021-09-17 02:10:39 +0300

    create programm

--- functions.php
+++ functions.php
@@ -1,2 +1,39 @@
 <?php
-?>
+function get_currency_by_abbreviation($val, $date=''){
+    if($val=='BYN'){
+        $result=[
+            'Cur_Abbreviation' => 'BYN',
+            'Cur_Conv'=>1
+        ];
+    } else {
+        $url = "https://www.nbrb.by/api/exrates/rates/$val?";
+        $params = [
+            "parammode" => 2,
+            "periodicity" => 0,
+            "ondate" => $date
+        ];
+        $params = http_build_query($params);
+        $response = file_get_contents($url . $params);
+        $currency = json_decode($response, true);
+        $result = [
+            'Cur_Abbreviation' => $currency['Cur_Abbreviation'],
+            'Cur_OfficialRate' => $currency['Cur_OfficialRate'],
+            'Cur_Scale' => $currency['Cur_Scale'],
+            'Cur_Conv' => ($currency['Cur_OfficialRate'] / $currency['Cur_Scale'])
+        ];
+    }
+    return($result);
+}
+
+if($_POST['money']){
+    $money = (int)(htmlentities($_POST['money']));
+    $date = (htmlentities($_POST['date'])) ?? "";
+    $val1 = (htmlentities($_POST['val1']));
+    $val2 = (htmlentities($_POST['val2']));
+    $val1 = get_currency_by_abbreviation($val1, $date);
+    $val2 = get_currency_by_abbreviation($val2, $date);
+    $result['curs']=round($val1['Cur_Conv']/$val2['Cur_Conv'], 2);
+    $result['sum'] = round($result['curs']*$money, 2);
+    echo $result['sum'] . $val2['Cur_Abbreviation'].', курс - ' . $result['curs'];
+}
+
--- index.php
+++ index.php
@@ -1,1 +1,90 @@
-<?php
+<?php ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Конвертер валют</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <style>
+        label{
+            float: left;
+            margin-right: 10px;
+        }
+        input {
+            height: 30px;
+            font-size: 2em;
+        }
+        select{
+            height: 36px;
+        }
+        #date{
+            padding: 1px;
+        }
+        #answer{
+            font-size: 2em;
+        }
+    </style>
+</head>
+<body>
+
+    <form method="post" >
+    <label>Дата <br>
+        <input id = "date" type = "date" >
+    </label>
+    <label>Валюта 1  <br>
+        <input id = "money" type = "number">
+    </label>
+    <label>  <br>
+        <select id="val1">
+            <option>BYN</option>
+            <option>RUB</option>
+            <option>USD</option>
+            <option>EUR</option>
+        </select>
+    </label>
+        <label>
+            <h2> > </h2>
+        </label>
+    <label> Валюта 2 <br>
+        <select id = "val2">
+            <option>BYN</option>
+            <option>RUB</option>
+            <option>USD</option>
+            <option>EUR</option>
+        </select>
+    </label>
+    </form><br>
+    <div id = "answer"></div>
+</body>
+<script>
+    $( document ).ready(function() {
+        $(function(){
+            $('select').change(function() {
+                $current=$(this);
+                $("select").not($current).children("option[value='"+$current.val()+"']").attr('disabled', "disabled");
+            });
+        });
+    });
+
+    $(function() {
+        $('#money').on('change', function(){
+            let date = $('#date').val();
+            let money = $('#money').val();
+            let val1 = $('#val1').val();
+            let val2 = $('#val2').val();
+            $.ajax({
+                method: "POST",
+                url: "functions.php",
+                data: {
+                    date: date,
+                    money: money,
+                    val1:val1,
+                    val2:val2
+                }
+            }).success(function(result){
+                $("#answer").html( result );
+            })
+        });
+    });
+</script>
+</html>
